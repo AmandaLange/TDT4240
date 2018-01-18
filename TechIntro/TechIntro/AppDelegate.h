467a3cab2c43c37a64f0f1c49669e398da09076c
rakelsa <rakelandreassen@gmail.com>
2018-01-18 15:41:33 +0100

    Exercise 1 - Rakel

---
+++ TechIntro/TechIntro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TechIntro
+//
+//  Created by Rakel Sjaastad Andreassen on 18.01.2018.
+//  Copyright © 2018 Rakel Sjaastad Andreassen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TechIntro/TechIntro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TechIntro
+//
+//  Created by Rakel Sjaastad Andreassen on 18.01.2018.
+//  Copyright © 2018 Rakel Sjaastad Andreassen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
